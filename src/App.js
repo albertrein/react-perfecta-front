00b2cd7e341fce78ceabf4afd6fdaf38504ed39d
albertrein <guilhermealberto8@gmail.com>
2020-01-17 18:15:59 -0200

    Remove unusual files.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Teste from './components/Teste';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,6 +17,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Teste />
     </div>
   );
 }
