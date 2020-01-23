6d0da71f9711e9276041d636e51dd3b81df65706
albertrein <guilhermealberto8@gmail.com>
2020-01-23 13:24:59 -0300

    Creating styles file.

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,9 +1,20 @@
 import React, {Component} from 'react';
+import "style.css";
 
 export default class Login extends Component{
     render(){
         return (
-            <div>Login</div>
+            <div class="login_content">
+                <div class="input_box">
+                    <span>Login:</span> 
+                    <input type="text" name="login" id="login" />
+                </div>
+                <div class="input_box">
+                    <span>Senha:</span> 
+                    <input type="text" name="pass" id="pass" />
+                </div>
+                <button>Entrar!</button>
+            </div>
         )
     }
 }
