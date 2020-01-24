9d9721adc04a2c12e772e5126a316f54a5a1a59c
albertrein <guilhermealberto8@gmail.com>
2020-01-24 13:22:45 -0300

    Creating Admin manager component.

---
+++ src/pages/admin/Admin.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+export default class Admin extends Component{
+    render(){
+        return(
+
+        )
+    }
+}
