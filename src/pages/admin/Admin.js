4cfe3fe2032497d81b8fd7bd69e1faed28bc49f4
albertrein <guilhermealberto8@gmail.com>
2020-01-25 20:34:05 -0300

    Adding swith routes

--- src/pages/admin/Admin.js
+++ src/pages/admin/Admin.js
@@ -5,7 +5,12 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 export default class Admin extends Component{
     render(){
         return(
-
+        	<BrowserRouter>
+        		<Switch>
+        			<Route path="/login" component={Login}/>
+        			<Route path="/admin" component={Admin}/>
+        		</Switch>
+        	</BrowserRouter>
         )
     }
 }
