2a1dcfdde0c0ef7e022ceb1b9ca9673436422cf5
albertrein <guilhermealberto8@gmail.com>
2020-01-26 23:01:31 -0300

    Creating sing-in in admin components.

--- src/pages/admin/AdminPage.js
+++ src/pages/admin/AdminPage.js
@@ -5,13 +5,28 @@ import Login from './components/Login';
 import Admin from './components/Admin';
 
 export default class AdminPage extends Component{
+	constructor(props){
+		super(props);
+		let isLogged = localStorage.getItem("isLogged");
+		console.warn('>>',isLogged);
+		if(isLogged === 'false' || isLogged == null){
+			console.warn('if')
+			this.props.history.push('/admin/login');
+		}else{
+			console.warn('else')
+			this.props.history.push('/admin');
+		}
+	}
+	componentDidMount(){
+		
+	}
     render(){
         return(
         	<div>Admin Component
 	        	<BrowserRouter>
 	        		<Switch>
 	        			<Route path="/admin/login" exact={true} component={Login}/>
-	        			<Route path="/admin/admin" exact={true} component={() => <Admin teste="guilherme" />} />
+	        			<Route path="/admin"  component={() => <Admin teste="guilherme" />} />
 	        		</Switch>
 	        	</BrowserRouter>
         	</div>
