2e48eacbd5903a92f74e4d487d520868fc98ea37
albertrein <guilhermealberto8@gmail.com>
2020-01-30 13:28:51 -0300

    Creating forms

---
+++ src/pages/admin/components/forms/JobForm.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class JobForm extends Component {
+    render(){
+        return(
+            <form>
+                <input type="text" />
+                <button onClick=""></button>
+            </form>
+        );
+    }
+}
