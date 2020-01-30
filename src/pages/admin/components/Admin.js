97da2d53bb8000bbb8a1d1a2ec75f61a583c6379
albertrein <guilhermealberto8@gmail.com>
2020-01-30 13:27:32 -0300

    Inserting forms in content

--- src/pages/admin/components/Admin.js
+++ src/pages/admin/components/Admin.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { BrowserRouter, Switch, Route, Link} from 'react-router-dom';
 import './style/Admin.css';
+import CategoryForm from './forms/CategoryForm';
 
 export default class Admin extends Component{
 	state = {};
@@ -23,9 +24,10 @@ export default class Admin extends Component{
 				</div>
 				<div class="column content">
 					<BrowserRouter>
-						<Link to="/admin">Página Inicial</Link>
-						<Link to="/categoria">Nova Categoria</Link>
-						<Link to="/vaga">Nova Vaga</Link>
+						<Switch>
+							<Route parth="/admin/newcategorie" exact="true" component={CategoryForm}></Route>
+							<Route parth="/admin/newjob" component={CategoryForm}></Route>
+						</Switch>
 					</BrowserRouter>
 				</div>
 			</div>
