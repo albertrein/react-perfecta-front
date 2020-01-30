cc8a50384cb1e696787c13b322adcc99d26d625d
albertrein <guilhermealberto8@gmail.com>
2020-01-29 21:51:57 -0300

    Insert fast access to login Link.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Teste from './components/Teste';
 import AdminPage from './pages/admin/AdminPage';
 import ReactDOM from 'react-dom';
 import "./style-main.css";
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Link to="/admin"><h2>Acessar Login</h2></Link>
         <Switch>
           <Route path="/sobre" exact="true" component={Teste} />
           <Route path="/admin" component={AdminPage} />
