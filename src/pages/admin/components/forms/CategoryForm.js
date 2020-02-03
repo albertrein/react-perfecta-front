88c8a5bd7b2dde308fb5924b57e3e3800677f9d5
albertrein <guilhermealberto8@gmail.com>
2020-02-03 13:29:20 -0300

    Formatting category form.

--- src/pages/admin/components/forms/CategoryForm.js
+++ src/pages/admin/components/forms/CategoryForm.js
@@ -4,6 +4,7 @@ export default class CategoryForm extends Component {
     render(){
         return(
             <div className="insert-form-content">
+                <p className="title">Nova Categoria</p>
                 <form className="content-form">
                     <input type="text" />
                     <button onClick="">Salvar!</button>
