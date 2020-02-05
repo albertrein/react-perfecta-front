415c3aa810229803aea42f792a5bb4ee8c85db52
albertrein <guilhermealberto8@gmail.com>
2020-02-04 22:45:26 -0300

    Implementting save categorie.

--- src/pages/admin/components/forms/CategoryForm.js
+++ src/pages/admin/components/forms/CategoryForm.js
@@ -1,14 +1,28 @@
 import React, {Component} from 'react';
+import PerfectaApi from '../../../../services/perfecta-api';
 
 export default class CategoryForm extends Component {
+    async saveCategory(){
+        let categorieTitle = document.querySelector('input').value;
+        if(!categorieTitle){
+            console.error('Não foi possivel registrar!');
+            return ;
+        }
+        //Implent here loading gif        
+        let out = await PerfectaApi.setNewCategory(categorieTitle);
+        if(out.data.OK === 'Sucess'){
+            alert('Saved!');
+        }
+    }
+
     render(){
         return(
             <div className="insert-form-content">
                 <p className="title">Nova Categoria</p>
-                <form className="content-form">
+                <div className="content-form">
                     <input placeholder="Título da Categoria ..." type="text" />
-                    <button onClick="">Salvar Categoria!</button>
-                </form>
+                    <button onClick={this.saveCategory}>Salvar Categoria!</button>
+                </div>
             </div>
         );
     }
