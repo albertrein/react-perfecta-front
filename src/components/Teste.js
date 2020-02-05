1dd179726f55b5a581464cd4a110ff928933ddb7
albertrein <guilhermealberto8@gmail.com>
2020-02-05 13:02:05 -0300

    Disable teste component.

--- src/components/Teste.js
+++ src/components/Teste.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import api from '../model/perfecta-api';
+//import api from '../model/perfecta-api';
 
 export default class Teste extends Component{
 	componentDidMount(){
-		let out = api.newCategory('testeReact');
-		console.log('>>',out);
+		
 	}
 	render(){
 		return( <div>Hello React</div> );
