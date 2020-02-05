4697f6c44abf4010acc4bc152ec870757603eb3d
albertrein <guilhermealberto8@gmail.com>
2020-02-04 22:57:38 -0300

    Trying inserntig data from jobs new

--- src/pages/admin/components/forms/JobForm.js
+++ src/pages/admin/components/forms/JobForm.js
@@ -1,18 +1,25 @@
 import React, {Component} from 'react';
+import PerfectaApi from '../../../../services/perfecta-api';
+
 
 export default class JobForm extends Component {
+    async saveJob(){
+        let out = await PerfectaApi.setNewJob();
+        console.log(out);
+    }
+
     render(){
         return(
             <div className="insert-form-content">
                 <p className="title">Nova Vaga</p>
-                <form className="content-form">
+                <div className="content-form">
                     <input placeholder="Título da Vaga ..." type="text" />
                     <input placeholder="Descrição da Vaga ..." type="text" />
                     <select placeholder="Categoria da Vaga ..." >
                     	<option>Categoria 1</option>
                     </select>
-                    <button onClick="">Salvar Vaga!</button>
-                </form>
+                    <button onClick={this.saveJob}>Salvar Vaga!</button>
+                </div>
             </div>
         );
     }
