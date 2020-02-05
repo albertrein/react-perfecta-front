96afd979b96d8ed0ee42f5412c49302df0dd11b1
albertrein <guilhermealberto8@gmail.com>
2020-02-04 23:01:53 -0300

    Inserting methods in class, to make inserts in database.

--- src/services/perfecta-api.js
+++ src/services/perfecta-api.js
@@ -1,12 +1,29 @@
 import axios from 'axios';
 
 class PerfectaAPI{
-	async newCategory(categoryName){
-		let out = await axios.post('https://perfecta-api-node-firebase.herokuapp.com/new/category/'+categoryName);
+	//const atribute - url base
+	urlBase = "https://perfecta-api-node-firebase.herokuapp.com/";
+	
+
+	async setNewCategory(categoryName){
+		let out = await axios.post(this.urlBase + 'new/category/' + categoryName);
 		return out;
 	}
 
-	teste(){console.log('teste');}
+	async setNewJob(){
+		
+
+		let out = await axios.post(this.urlBase + 'new/job/', {
+			"jobCategory" : "Teste",
+			"jobTitle" : "Teste #1",
+			"jobDescription" : "Teste React Front #1"
+		});
+		//let out = await axios.post(this.urlBase + 'new/job/');
+		return out;
+	}
+
+
+	teste(){console.log('teste',this.urlBase);}
 }
 
 export default new PerfectaAPI();
