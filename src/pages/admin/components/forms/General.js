74f226e200725adea4c017f3086f33b306805d3c
albertrein <guilhermealberto8@gmail.com>
2020-02-09 23:03:34 -0300

    Printing categories and getting jobsByCategory.

--- src/pages/admin/components/forms/General.js
+++ src/pages/admin/components/forms/General.js
@@ -1,10 +1,28 @@
 import React, {Component} from 'react';
+import PerfectaApi from '../../../../services/perfecta-api';
+
 
 export default class General extends Component {
+    constructor(props){
+    	super(props);
+    	this.state = {
+    		allJobs: []
+    	}
+    }
+
+    async componentDidMount(){
+    	this.props.arrayCategories.map(async value => {
+	    	let out = await PerfectaApi.getJobByCategorie(value);
+    		console.log(out.data);
+    	});
+
+    }
+
     render(){
         return(
             <div className="insert-form-content">
                 <h1>General</h1>
+                {this.props.arrayCategories.map((value, i) => <p key={i}>{value}</p>)}
             </div>
         );
     }
