9861fd9aa3d2a49e3a950bc3bd456199b9daf711
albertrein <guilhermealberto8@gmail.com>
2020-02-10 23:29:00 -0300

    Making array object to use in props.

--- src/pages/admin/components/forms/General.js
+++ src/pages/admin/components/forms/General.js
@@ -11,18 +11,47 @@ export default class General extends Component {
     }
 
     async componentDidMount(){
+    	let arrayAll = [];
     	this.props.arrayCategories.map(async value => {
 	    	let out = await PerfectaApi.getJobByCategorie(value);
-    		console.log(out.data);
-    	});
+	    	let object = {
+	    		jobTitle: '',
+	    		arrayJobs: []
+	    	};
 
+	    	object.jobTitle = value;
+
+	    	if(out.data === ""){
+	    		object.arrayJobs = [];	
+	    	}else{
+	    		object.arrayJobs = Object.keys(out.data);
+	    	}
+
+	    	arrayAll.push(object);
+    	});
+    	this.setState({"allJobs": arrayAll});
     }
 
+
+
+    /*async teste(categorieTitle){
+    	let jobsByCategorie = await PerfectaApi.getJobByCategorie(categorieTitle);
+    	return Object.entries(jobsByCategorie.data);
+    	//console.log('>>',jobsByCategorie);
+    }
+    teste2(v){
+    	let out = this.teste(v);
+    	if(out === ""){
+    		return [];
+    	}
+    	return out; 
+    }*/
     render(){
         return(
             <div className="insert-form-content">
                 <h1>General</h1>
                 {this.props.arrayCategories.map((value, i) => <p key={i}>{value}</p>)}
+                {this.state.allJobs.map((val, i) => <a key={i}>{val.jobTitle}</a>)}
             </div>
         );
     }
