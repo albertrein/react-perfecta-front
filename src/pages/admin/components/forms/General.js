9ec4c338add96d68286b20bcb2a0e26a13727ff7
albertrein <guilhermealberto8@gmail.com>
2020-02-12 08:42:23 -0300

    Make map inside map.

--- src/pages/admin/components/forms/General.js
+++ src/pages/admin/components/forms/General.js
@@ -6,35 +6,31 @@ export default class General extends Component {
     constructor(props){
     	super(props);
     	this.state = {
-    		allJobs: [
-    			{jobTitle: "Gerenciamento"},
-				{jobTitle: "Teste"},
-				{jobTitle: "Marketing"},
-				{jobTitle: "teste"},
-				{jobTitle: "TI"},
-				{jobTitle: "qweqwe"}
-    		],
+    		allJobs: [],
     		teste1: ''
-    	}
-    	this.teste = this.teste.bind(this);
-    }
+		}		
+	}
+	
+	componentDidMount(){
+		setTimeout(() => {
+			this.setState({
+				allJobs: [
+					{ id: '1', age: [{title: "t1"}, {title: "t2"}] },
+					{ id: '2', age: [{title: "t3"}, {title: "t4"}] },
+					{ id: '3', age: [{title: "t5"}, {title: "t6"}] }
+				]
+			});
+		}, 4000);
+		
+	}
 
-    async componentDidMount(){
-    	await this.teste();
-    	console.log('>',this.state.allJobs);
-    }
-
-    async teste(){
+/*	async componentDidMount(){
     	let arrayAll = [];
     	this.props.arrayCategories.map(async value => {
 	    	let out = await PerfectaApi.getJobByCategorie(value);
 	    	let object = {
 	    		jobTitle: '',
-	    		arrayJobs: [
-	    			{ id: '1', age: 42 },
-        			{ id: '2', age: 33 },
-        			{ id: '3', age: 68 },
-	    		]
+	    		arrayJobs: []
 	    	};
 
 	    	object.jobTitle = value;
@@ -47,34 +43,17 @@ export default class General extends Component {
 
 	    	arrayAll.push(object);
     	});
-    	//console.log(arrayAll)
-    	//await this.setState({"allJobs": arrayAll});
-    	//this.setState({teste1: 'guilherme'});
-    }
-
-    /*temp1.map(v => {
-    	console.log(v.jobTitle)
-    	v.arrayJobs.map(vv => console.log(vv))
-	})*/
-
-    /*async teste(categorieTitle){
-    	let jobsByCategorie = await PerfectaApi.getJobByCategorie(categorieTitle);
-    	return Object.entries(jobsByCategorie.data);
-    	//console.log('>>',jobsByCategorie);
-    }
-    teste2(v){
-    	let out = this.teste(v);
-    	if(out === ""){
-    		return [];
-    	}
-    	return out; 
+    	this.setState({"allJobs": arrayAll});
     }*/
+
+
     render(){
         return(
             <div className="insert-form-content">
                 <h1>General</h1>
-                <p>{this.state.teste1}</p>
-                {this.state.allJobs.filter((vv) => <p key={vv.id}>{vv.age}</p>)}
+                {this.state.allJobs.map((vv) => {
+					return <p key={vv.id}>{vv.age.map((vvv) => <a key={vvv.title}>{vvv.title}</a> )}</p>
+				})}
             </div>
         );
     }
