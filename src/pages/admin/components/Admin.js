f3652410ef977f12c8a4ff944232d8d38ed3633e
albertrein <guilhermealberto8@gmail.com>
2020-02-16 21:44:12 -0300

    Removing unusual react router methods

--- src/pages/admin/components/Admin.js
+++ src/pages/admin/components/Admin.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+import {Route, Link} from 'react-router-dom';
 import './style/Admin.css';
 import CategoryForm from './forms/CategoryForm';
 import JobForm from './forms/JobForm';
