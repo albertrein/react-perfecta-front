bd9609692596d42c9691a337950b5ecf22cb3747
albertrein <guilhermealberto8@gmail.com>
2020-02-20 23:51:34 -0300

    Making home components.

---
+++ src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import Head from './components/Head';
+
+
+export default class Home extends Component{
+	render(){
+		return (
+			<Head />
+			//Here all future containers of components
+		);
+	}
+}
---
+++ src/pages/home/components/Head.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import Banner01 from '../../../assets/uploads/banner-01.jpg';
+
+export default class Home extends Component{
+	render(){
+		return (
+			<section id="home" className="main-banner parallaxie" style={{backgroundImage: 'url(${Banner01})' }}>
+				<div className="heading">
+					<h1>Welcome to OnNext</h1>			
+					<h3 className="cd-headline clip is-full-width">
+						<span>Lorem Ipsum Dolor Sit Amet </span>
+						<span className="cd-words-wrapper">
+							<b className="is-visible">Web Developer</b>
+							<b>Web Design</b>
+							<b>Creative Design</b>
+							<b>Graphic Design</b>
+						</span>
+						<div className="btn-ber">
+							<a className="get_btn hvr-bounce-to-top" href="#">Get started</a>
+							<a className="learn_btn hvr-bounce-to-top" href="#">Learn More</a>
+						</div>
+					</h3>
+				</div>
+			</section>
+		)
+	}
+}
