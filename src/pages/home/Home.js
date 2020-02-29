594e4a0a8919d116d8d9dbf6f72934d815029ecb
albertrein <guilhermealberto8@gmail.com>
2020-02-29 10:58:24 -0300

    Seting redirect to new home page.

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,10 +2,14 @@ import React, {Component} from 'react';
 
 
 export default class Home extends Component{
+	mekeRedirectToFrontPage(){
+		//Here set location to website front home page
+		window.location.href = "http://github.com/albertrein";
+	}
 	render(){
 		return (
 			<div className="container-home">
-				
+				<h1>Redirecionando para página inicial do site ...</h1>
 			</div>
 		);
 	}
