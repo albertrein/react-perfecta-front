534ced4202553429ec2b4d46784adc14f5da0293
albertrein <guilhermealberto8@gmail.com>
2020-02-29 11:10:56 -0300

    Removing template files.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Switch>
           <Route path="/" exact="true" component={Home} />
           <Route path="/admin" component={AdminPage} />
-        </Switch>      
+        </Switch>
       </BrowserRouter>
     </div>
   );
